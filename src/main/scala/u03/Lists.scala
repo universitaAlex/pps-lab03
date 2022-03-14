40f2e08f5a9c6f2bfdc23449d6964ff90d7347c1
alexbaiardi <alex.baiardi99@gmail.com>
2022-03-14 12:16:49 +0100

    Lab activity

---
+++ src/main/scala/u03/Lists.scala
@@ -0,0 +1,61 @@
+package u03
+
+object Lists extends App :
+
+  // A generic linkedlist
+  enum List[E]:
+    case Cons(head: E, tail: List[E])
+    case Nil()
+
+  // a companion object (i.e., module) for List
+  object List:
+
+    def sum(l: List[Int]): Int = l match
+      case Cons(h, t) => h + sum(t)
+      case _ => 0
+
+    def map[A, B](l: List[A])(mapper: A => B): List[B] = l match
+      case Cons(h, t) => Cons(mapper(h), map(t)(mapper))
+      case Nil() => Nil()
+
+    def filter[A](l1: List[A])(pred: A => Boolean): List[A] = l1 match
+      case Cons(h, t) if pred(h) => Cons(h, filter(t)(pred))
+      case Cons(_, t) => filter(t)(pred)
+      case Nil() => Nil()
+
+    def drop[A](l: List[A])(n: Int): List[A] = l match
+      case Cons(h, t) if n > 0 => drop(t)(n - 1)
+      case _ => l
+
+    def append[A](left: List[A])(right: List[A]): List[A] = left match
+      case Cons(h, t) => Cons(h, append(t)(right))
+      case _ => right
+
+    def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] = l match
+      case Cons(h, t) => append(f(h))(flatMap(t)(f))
+      case _ => Nil()
+
+    def mapWithFlatMap[A, B](l: List[A])(mapper: A => B): List[B] =
+      flatMap(l)(v => Cons(mapper(v), Nil()))
+
+    def filterWithFlatMap[A](l: List[A])(pred: A => Boolean): List[A] =
+      val flatMapper: A => List[A] = v => v match
+        case v if pred(v) => Cons(v, Nil())
+        case _ => Nil()
+      flatMap(l)(flatMapper)
+
+    def max(l: List[Int]): Option[Int] = l match
+      case Cons(h, t) if h > (max(t) match {
+        case Some(n) => n
+        case _ => -Int.MinValue
+      }) => Option(h)
+      case Cons(h, t) => max(t)
+      case _ => Option.empty
+
+
+  val l = List.Cons(10, List.Cons(20, List.Cons(30, List.Nil())))
+  println(List.sum(l)) // 60
+
+  import List.*
+
+  println(sum(map(filter(l)(_ >= 20))(_ + 1))) // 21+31 = 52
---
+++ src/test/scala/u03/ListTest.scala
@@ -0,0 +1,50 @@
+package u03
+
+import org.junit.*
+import org.junit.Assert.*
+import Lists.*
+
+class ListTest:
+
+  import List.*
+
+  val l: List[Int] = Cons(10, Cons(20, Cons(30, Nil())))
+
+  @Test def testSum() =
+    assertEquals(0, sum(Nil()))
+    assertEquals(60, sum(l))
+
+  @Test def testMap() =
+    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), map(l)(_ + 1))
+    assertEquals(Cons("10", Cons("20", Cons("30", Nil()))), map(l)(_ + ""))
+
+  @Test def testFilter() =
+    assertEquals(Cons(20, Cons(30, Nil())), filter(l)(_ >= 20))
+    assertEquals(Cons(10, Cons(30, Nil())), filter(l)(_ != 20))
+
+  @Test def testDrop() =
+    assertEquals(Cons(20, Cons(30, Nil())), drop(l)(1))
+    assertEquals(Cons(30, Nil()), drop(l)(2))
+    assertEquals(Nil(), drop(l)(5))
+
+  @Test def testAppend() =
+    val tail = Cons(40, Nil())
+    assertEquals(Cons(10, Cons(20, Cons(30, Cons(40, Nil())))), append(l)(tail))
+
+  @Test def testFlatMap() =
+    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), flatMap(l)(v => Cons(v + 1, Nil())))
+    assertEquals(Cons(11, Cons(12, Cons(21, Cons(22, Cons(31, Cons(32, Nil())))))), flatMap(l)(v => Cons(v + 1, Cons(v + 2, Nil()))))
+
+  @Test def testMapWithFlapMap() =
+    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), mapWithFlatMap(l)(_ + 1))
+    assertEquals(Cons("10", Cons("20", Cons("30", Nil()))), mapWithFlatMap(l)(_ + ""))
+
+  @Test def testFilterWithFlatMap() =
+    assertEquals(Cons(20, Cons(30, Nil())), filterWithFlatMap(l)(_ >= 20))
+    assertEquals(Cons(10, Cons(30, Nil())), filterWithFlatMap(l)(_ != 20))
+
+  @Test def testMax() =
+    assertEquals(Some(25), max(Cons(10, Cons(25, Cons(20, Nil())))))
+    assertEquals(None, max(Nil()))
+
+  @Test def testMapPersonInCourses()=
